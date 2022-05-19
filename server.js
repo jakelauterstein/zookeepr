e868abac02c8b98fe1915c44a5e61dbc285d479a
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-05-19 13:42:21 -0500

    added POST route

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 function filterByQuery(query, animalsArray) {
@@ -48,6 +55,36 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
+  function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+  
+      // return finished code to post route for response
+    return animal;
+  }
+
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
 // route for GET request, two arguments, 1. the route the client wil fetch from, 2. the callback function that executes with each GET request
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -65,6 +102,23 @@ app.get('/api/animals/:id', (req, res) => {
       res.send(404);
     }
   });
+
+  // route that listens for POST requests
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+      req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+      res.json(req.body);
+  }
+  });
+
 
 
 app.listen(PORT, () => {
